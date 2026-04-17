0978d4e80fcda4833b16e9e6b156f4cb13576d14
pskart077-creator <pskart077@gmail.com>
2026-04-17 17:44:41 -0300

    chore: sync ajustes pendentes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Workaround para ambientes Windows com lock intermitente em ".next/trace"
-  distDir: "build"
 };
 
 export default nextConfig;
